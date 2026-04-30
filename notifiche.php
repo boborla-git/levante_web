4dd984cceb5c3e96b88255a684cacf9f987e6b4b
boborla-git <boborla@gmail.com>
2026-04-30 20:08:53 +0200

    Protegge pagina notifiche con sistema permessi

--- notifiche.php
+++ notifiche.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/includes/db.php';
 require_once __DIR__ . '/includes/auth.php';
 require_once __DIR__ . '/includes/layout.php';
 
-richiediLogin();
+richiediPermessoLettura('notifiche');
 
 $pdo = db();
 $idUtente = (int)($_SESSION['id_utente'] ?? $_SESSION['utente_id'] ?? 0);
